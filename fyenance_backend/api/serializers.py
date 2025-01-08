f73136b8104b99e7434ef04e35a31e127c41ef95
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2025-01-08 09:11:07 -0500

    Changed Duplicate Serializer definitions

--- fyenance_backend/api/serializers.py
+++ fyenance_backend/api/serializers.py
@@ -1,20 +1,9 @@
-from .models import BaseTransaction, Budget, Category
+from .models import FyenanceUser
 from rest_framework import serializers
 
-
-class TransactionSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    
     class Meta:
-        model = BaseTransaction
-        fields = ('transaction_id', 'amount', 'type', 'category', 'user')
-        extra_kwargs = {'user': {'read_only': True}}
-
-class BudgetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Budget
-        fields = ('name', 'amount_allocated', 'amount_left', 'user')
-        extra_kwargs = {'user': {'read_only': True}}
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('name', 'colour')
+        model = FyenanceUser
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = { 'password': {'write_only': True}}
