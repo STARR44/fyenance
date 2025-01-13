6de4fff19bc3e474b493c5beb04d8f98c93525c9
STARR44 <okekeesther100@gmail.com>
2025-01-13 13:30:52 +0100

    chore: import dropdown menu into the maindashboard component

--- src/pages/DashboardPages/MainDashboard.js
+++ src/pages/DashboardPages/MainDashboard.js
@@ -1,5 +1,5 @@
 // src/pages/DashboardPages/MainDashboard.js
-// src/pages/DashboardPages/MainDashboard.js
+
 import React, { useState } from "react"; // Removed axios and useEffect as APIs are no longer needed
 import {
   DashboardNav, // Navigation bar component for the dashboard
@@ -8,6 +8,7 @@ import {
   NewUserPlaceholder, // Placeholder content for new users
 } from "../../components/DashboardComponents";
 import { FaPlus } from "react-icons/fa"; // Import the plus icon from react-icons
+import DropdownMenu from "../../components/DashboardComponents/DropdownMenu";
 import DashboardCards from "../../components/DashboardComponents/DashboardCards"; // Import the new component
 import OverlaySpinner from "../../components/DashboardComponents/OverlaySpinner"; // Spinner component for loading state
 import "./MainDashboard.css"; // Import styles specific to the dashboard page
@@ -24,6 +25,9 @@ function DashboardPage() {
     addBudget: false, // Tracks if the 'Add Budget' action is done
     linkBankAccount: false, // Tracks if the 'Link Bank Account' action is done
   });
+
+  // State to control dropdown visibility for the Add button for old users
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
 
   // Check if any action has been completed (for determining user status)
   const hasActions = Object.values(actionsCompleted).some(
@@ -55,10 +59,18 @@ function DashboardPage() {
           <div>
             <div className="greeting-and-add-container">
               <Greeting user={user} />
-              <button className="add-button">
+              <button
+                className="add-button"
+                onClick={() => setDropdownVisible(!isDropdownVisible)} // Toggle dropdown for Add Button
+              >
                 Add
                 <FaPlus size={18} className="plus-icon" />
               </button>
+              {/* Pass the state and function to DropdownMenu for the Add button */}
+              <DropdownMenu
+                isDropdownVisible={isDropdownVisible}
+                setDropdownVisible={setDropdownVisible}
+              />
             </div>
 
             <DashboardCards
