f668474d7d06beadca23610ff3c9d1a19dfca6e4
STARR44 <okekeesther100@gmail.com>
2025-01-21 21:32:30 +0100

    feat: add global context provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { GlobalProvider } from "./context/GlobalContext";
 import { CurrencyProvider } from "./context/currencyContext"; // Import CurrencyProvider to manage global currency state
 import LandingPage from "./pages/LandingPage"; // Import Landing page
 import About from "./pages/AboutPage"; // Import About page
@@ -10,34 +11,36 @@ import SettingsPage from "./pages/DashboardPages/SettingsPages/Settings"; // Imp
 
 function App() {
   return (
-    <CurrencyProvider>
-      {" "}
-      {/* Wrap the app with CurrencyProvider to make the currency state accessible */}
-      <Router>
-        <div>
-          {/* Define the routes */}
-          <Routes>
-            {/* The LandingPage component is displayed for the root path */}
-            <Route path="/" element={<LandingPage />} />
+    <GlobalProvider>
+      <CurrencyProvider>
+        {" "}
+        {/* Wrap the app with CurrencyProvider to make the currency state accessible */}
+        <Router>
+          <div>
+            {/* Define the routes */}
+            <Routes>
+              {/* The LandingPage component is displayed for the root path */}
+              <Route path="/" element={<LandingPage />} />
 
-            {/* The About component is displayed for the /about path */}
-            <Route path="/about" element={<About />} />
+              {/* The About component is displayed for the /about path */}
+              <Route path="/about" element={<About />} />
 
-            {/* The Dashboard component is displayed for the /dashboard path */}
-            <Route path="/dashboard" element={<DashboardPage />} />
+              {/* The Dashboard component is displayed for the /dashboard path */}
+              <Route path="/dashboard" element={<DashboardPage />} />
 
-            {/* The Dashboard component is displayed for the /transaction page path */}
-            <Route path="/transactions" element={<TransactionPage />} />
+              {/* The Dashboard component is displayed for the /transaction page path */}
+              <Route path="/transactions" element={<TransactionPage />} />
 
-            {/* The Dashboard component is displayed for the /Budget page path */}
-            <Route path="/budget" element={<BudgetPage />} />
+              {/* The Dashboard component is displayed for the /Budget page path */}
+              <Route path="/budget" element={<BudgetPage />} />
 
-            {/* The Settings component is displayed for the /settings path */}
-            <Route path="/settings" element={<SettingsPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </CurrencyProvider>
+              {/* The Settings component is displayed for the /settings path */}
+              <Route path="/settings" element={<SettingsPage />} />
+            </Routes>
+          </div>
+        </Router>
+      </CurrencyProvider>
+    </GlobalProvider>
   );
 }
 
